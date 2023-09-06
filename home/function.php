af002cc33d42ffa402cb3173d5236b841c7bc8ca
kinoppol <kinoppol@hotmail.com>
2023-09-06 21:48:23 +0700

    exc

---
+++ home/function.php
@@ -0,0 +1,4 @@
+<?php
+function welcome(){
+  print "Welcome to DVE-API";
+}
--- index.php
+++ index.php
@@ -1,2 +1,12 @@
 <?php
 require_once('limonade/lib/limonade.php');
+require_once('../dve-api-config/dve-api.cfg.php');
+$APP_NAME=$_POST['app'];
+$TOKEN=$_POST['token'];
+$chk_token=md5($APP_NAME.$SEED);
+if($TOKEN!=$chk_token){
+  die('Access denined.');
+}
+dispatch('/','welcome');
+require_once('home/function.php');
+run();
